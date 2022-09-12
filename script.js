86ec9d97cb7f9d45b3fde5cf3f69cf6bc0187164
novruzzade <tunarnovruzzad54@gmail.com>
2022-09-13 00:41:53 +0400

    Updated

---
+++ script.js
@@ -0,0 +1,5 @@
+function background() {
+    document.querySelector(".text-box").style = "opacity: 1";
+    document.querySelector(".btn-box").style = "opacity: 1";
+    document.querySelector(".overlay").style = "background-color: rgb(0 0 0 / 42%)";
+}
